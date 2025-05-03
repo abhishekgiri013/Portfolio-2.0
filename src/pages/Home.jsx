1e671ed1fb671740cf2dd7475bdeee7ccfb5789d
ABHISHEK GIRI <98091185+abhishekgiri013@users.noreply.github.com>
2025-05-03 19:12:05 +0530

    first commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+import { ThemeToggle } from "../components/ThemeToggle";
+import { StarBackground } from "../components/StarBackground";
+import { Navbar } from "../components/Navbar";
+import { HeroSection } from "../components/HeroSection";
+import { AboutSection } from "../components/AboutSection";
+import { SkillsSection } from "../components/SkillsSection";
+import { ProjectsSection } from "../components/ProjectsSection";
+import { ContactSection } from "../components/ContactSection";
+import { Footer } from "../components/Footer";
+
+export default function Home() {
+  return (
+    <div className="min-h-screen">
+      {/*Theme Toggle */}
+      <ThemeToggle />
+      {/*Background Effects */}
+      <StarBackground />
+
+      {/*Navbar*/}
+      <Navbar />
+
+      {/*Main Content */}
+      <HeroSection />
+
+      {/*About */}
+      <AboutSection />
+
+      {/*Skills */}
+      <SkillsSection />
+      
+      {/*Projects */}
+      <ProjectsSection />
+
+      {/*Contact */}
+      <ContactSection />
+
+      {/*Footer */}
+      <Footer />
+    </div>
+  );
+}
